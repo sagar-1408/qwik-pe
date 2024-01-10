32905d579a6b54be95244d9647f979bd02786608
Sagar Dhrangdhariya <sagar.dhrangdhariya@gmail.com>
2024-01-10 17:09:51 +0530

    Added header, footer, and shared compoents

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [],
+  providers: [],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA,
+    NO_ERRORS_SCHEMA
+  ]
+})
+export class SharedModule { }
